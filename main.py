a9448b3390cb73e540a5b6f5ce48888cda6fe924
Romeo Disca <romeo.disca@gmail.com>
2024-01-04 22:52:46 +0100

    roc finished

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 
 from math import inf
 import numpy as np
+import pandas as pd
 import matplotlib
 matplotlib.use("GTK4Agg")
 import matplotlib.pyplot as plt
@@ -11,57 +12,19 @@ from sklearn.metrics import RocCurveDisplay
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
-# def roc(true_labels, predictions):
-#     """
-#     -------------------------------------------------------------------------------------------------------------------------------
-#     Args:
-#         true_labels: numpy array of true labels, positive class is signaled by a value of 1, any other value is assumed as negative class
+def roc(true_labels, predictions):
 
-#         predictions: model class predictions. Higher values denote more likelihood of sample belonging to the positive class
+    condition = lambda x: 1 if x >= 0 else 0
 
-#     Returns:
-#         false positive rate and corresponding true positive rate arrays.
-#     -------------------------------------------------------------------------------------------------------------------------------
-#     """
+    df = pd.DataFrame(np.array([true_labels, predictions]).T, columns=['true_labels', 'predictions'])
+    df['predictions'] = df['predictions'].map(condition)
 
-#     POSITIVE = 1
+    tp = len(df[(df['true_labels'] == 1) & (df['predictions'] == 1)])
+    tn = len(df[(df['true_labels'] == 0) & (df['predictions'] == 0)])
+    fp = len(df[(df['true_labels'] == 0) & (df['predictions'] == 1)])
+    fn = len(df[(df['true_labels'] == 1) & (df['predictions'] == 0)])
 
-#     # 1. count total number of positive salmples
-
-#     # 2. count total number of negative sample
-
-#     # 3. find indices that sort the predictions array
-
-#     # 4. sort the same way the predictions and the true_labels
-
-#     f_prev = -inf
-
-#     # 5. define a way to store the false positive and true positive rates
-
-
-#     # keep counts 
-#     fp = 0.0
-
-#     tp = 0.0
-
-#     while idx < num_examples:
-
-#         # 6. condition to decide if the false positive and true positive rates should be updated
-#         if <CONDITION>:
-
-#             # 7. update important quantities
-
-#         if # 8. condition to update fp and tp counts:
-#             tp = <CODE>
-#         else:
-#             fp = <CODE>
-
-#         idx += 1
-
-#     # 9. don't miss a case here, bewew of +/- 1 count errors  
-
-#     return # false positive rate and true positive rate
-
+    return fp/(fp + tn), tp/(tp + fn)
 
 # def roc_conceptual(true_labels, predictions, number_of_thresholds=1000):
 
@@ -127,15 +90,16 @@ if __name__ == '__main__':
 
     # plot and display the sklearn ROC and AUC results
 
-    RocCurveDisplay.from_predictions(y_test, y_pred,name="sklearn")
+    RocCurveDisplay.from_predictions(y_test, y_pred, name="sklearn")
     
     # calls your roc and auc implementations
 
-    # fpr, tpr = roc(y_test, y_pred)
+    fpr, tpr = roc(y_test, y_pred)
+    print(fpr, tpr)
     # roc_auc_aduh = auc_roc(y_test, y_pred)
     # impl = RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc_aduh, estimator_name="Lerner")
     # impl.plot()
 
-    plt.ion()
-    plt.show()
-    plt.waitforbuttonpress()
+    # plt.ion()
+    # plt.show()
+    # plt.waitforbuttonpress()
