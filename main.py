435c60719501c77274d3a4770ac2503590493d07
Romeo Disca <romeo.disca@gmail.com>
2024-01-05 03:56:22 +0100

    finished roc curve + auc

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def roc(true_labels, predictions):
     actual = np.array(true_labels)
     y_pred = np.array(predictions)
 
-    idxs = np.argsort(predictions)[::-1]
+    idxs = np.argsort(predictions)
     
     actual = actual[idxs]
     y_pred = y_pred[idxs]
@@ -36,27 +36,27 @@ def roc(true_labels, predictions):
         tpr.append(tp / (tp + fn))
         fpr.append(fp / (fp + tn))
 
-    return tpr, fpr
+    return fpr, tpr
 
 
 
-# def auc_roc(true_labels, predictions):
-#     """
-#     -------------------------------------------------------------------------------------------------------------------------------
-#     Args:
-#         true_labels: numpy array of true labels, positive class is signaled by a value of 1, any other value is assumed as negative class
+def auc_roc(true_labels, predictions):
+    """
+    -------------------------------------------------------------------------------------------------------------------------------
+    Args:
+        true_labels: numpy array of true labels, positive class is signaled by a value of 1, any other value is assumed as negative class
 
-#         predictions: model class predictions. Higher values denote more likelihood of sample belonging to the positive class
+        predictions: model class predictions. Higher values denote more likelihood of sample belonging to the positive class
 
-#     Returns:
-#         AUC Area under de ROC curve.
-#     -------------------------------------------------------------------------------------------------------------------------------
-#     """
+    Returns:
+        AUC Area under de ROC curve.
+    -------------------------------------------------------------------------------------------------------------------------------
+    """
 
-#     # 1. get the false and true positive rates
-#     fpr, tpr = roc(true_labels, predictions)
+    # 1. get the false and true positive rates
+    fpr, tpr = roc(true_labels, predictions)
 
-#     return # 2. use a numpy function to perfom the numerical integration
+    return 1 + np.trapz(fpr, tpr)
 
 
 if __name__ == '__main__':
@@ -75,11 +75,10 @@ if __name__ == '__main__':
     # calls your roc and auc implementations
 
     fpr, tpr = roc(y_test, y_pred)
-    print(fpr, tpr)
-    # roc_auc_aduh = auc_roc(y_test, y_pred)
-    # impl = RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc_aduh, estimator_name="Lerner")
-    # impl.plot()
+    roc_auc_aduh = auc_roc(y_test, y_pred)
+    impl = RocCurveDisplay(fpr=fpr, tpr=tpr, roc_auc=roc_auc_aduh, estimator_name="Lerner")
+    impl.plot()
 
-    # plt.ion()
-    # plt.show()
-    # plt.waitforbuttonpress()
+    plt.ion()
+    plt.show()
+    plt.waitforbuttonpress()
